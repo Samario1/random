ef37e2764c652045b4aac8d3b8226239c6645247
MRokas <rokasmuningis@gmail.com>
2015-07-21 19:36:16 +0300

    Update rps.php

--- rps.php
+++ rps.php
@@ -16,13 +16,15 @@ $win = array(
 );
 $playerpick = isset($arg[0])?$arg[0]:die("$sender - you have to pick one of these, to play this game: ".implode(", ",array_map("ucfirst",$picks)));
 $botpick = $picks[array_rand($picks)];
-  if ($playerpick == $botpick){
-    echo "$sender and $bot both picked $playerpick and tied!";
-  }
-  else if (isset($win[$playerpick."_".$botpick])){
+  if (isset($win[$playerpick."_".$botpick])){
     echo "$sender picked $playerpick and ".$win[$playerpick."_".$botpick]." $bot's $botpick ";
   } else if (isset($win[$botpick."_".$playerpick])){
     echo "$bot picked $botpick and ".$win[$botpick."_".$playerpick]." $sender's $playerpick ";
+  }else if ($playerpick == $botpick){
+    echo "$sender and $bot both picked $playerpick and tied!";
+  } else if(in_array($playerpick,$picks) && in_array($botpick,$picks)){
+    echo "$sender's $playerpick tied against $bot's $botpick";
+  }
   } else {
     echo "$sender - you have to pick one of these, to play this game: ".implode(", ",array_map("ucfirst",$picks));
   }

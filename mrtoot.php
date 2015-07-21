44dc892c86b311e77237ae93cc9d2c3be939b048
MRokas <rokasmuningis@gmail.com>
2015-07-21 15:16:07 +0300

    Create mrtoot.php

---
+++ mrtoot.php
@@ -0,0 +1,22 @@
+<?php
+
+  $arr = array(
+    "Lady kissed him on the belly",
+    "Children dressed like him in school",
+    "He ate caviar and jelly",
+    "Bought a modern swimming pool",
+    "Yes, Mr. Toot got gready",
+    "Wanted even better Toot",
+    "Threw the Toot from highest tower",
+    "Bought a fancy MIDI Toot",
+    
+    "Then a poster in the city",
+    "Mr. Ibanez tonight",
+    "Modern show with modern music",
+    "Smoke machine and laser light",
+    "Play it Mr.Toot",
+    "Thought you can kill the man
+      You can never kill the Toot"
+  );
+  die($arr[array_rand($arr)]);
+?>

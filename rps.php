3f01d746bb21f6f0181ec3be92fe7e6edd11750b
MRokas <rokasmuningis@gmail.com>
2015-07-22 20:07:46 +0300

    Update rps.php

--- rps.php
+++ rps.php
@@ -18,7 +18,7 @@ $win = array(
 );
 $playerpick = isset($arg[0])?$arg[0]:die("$sender - you have to pick one of these, to play this game: ".implode(", ",array_map("ucfirst",$picks)));
 $playerpick=="help"?die("Welcome to RPS Shocky edition. You can see the code at: https://github.com/MRokas/random/blob/master/rps.php And if you want to play, you have to pick one of these: ".implode(", ",array_map("ucfirst",$picks))):"";
-if(rand(0,5)==rand(0,5)){
+if(rand(0,3)==rand(0,3)){
   die($bot.' won. He just won, don\'t ask how.'); // to be changed to actual cheated win.
 }
 $botpick = $picks[array_rand($picks)];

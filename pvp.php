bfb7293e04f3001a9a8303c8863fe7b79d182389
MRokas <rokasmuningis@gmail.com>
2015-07-22 15:26:03 +0300

    Update pvp.php

--- pvp.php
+++ pvp.php
@@ -2,6 +2,9 @@
 if(!isset($arg[0])){
 	die($sender.' : you\'ve to choose enemy!');
 }
+if($arg[0] == $bot){
+	die($bot.'dealt 9001 damage and killed'.$sender);
+}
 
 $player1 = array(
 	"name"	=> $sender,
@@ -19,9 +22,9 @@ $player1['dealtdmg'] = $player1['dmg'] - $player2['def']*$defp;
 $player2['dealtdmg'] = $player2['dmg'] - $player1['def']*$defp;
 
 if($player1['dealtdmg'] > $player2['dealtdmg']){
-	echo $player1['name'].'dealt '.$player1['dmg'].' and killed '.$player2['name'].'!';
+	echo $player1['name'].' dealt '.$player1['dmg'].'damage and killed '.$player2['name'].'!';
 }else if($player2['dealtdmg'] > $player1['dealtdmg']){
-	echo $player2['name'].'dealt '.$player2['dmg'].' and killed '.$player1['name'].'!';
+	echo $player2['name'].' dealt '.$player2['dmg'].'damage and killed '.$player1['name'].'!';
 }else if($player1['dealtdmg'] == $player2['dealtdmg']){
 	echo $player1['name'].' and '.$player2['name'].' tied in the battle!';
 }

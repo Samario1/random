450eca44a4a35eee732cae35fbcfadc8b17cab5c
MRokas <rokasmuningis@gmail.com>
2015-07-22 21:48:10 +0300

    Update prism.php

--- prism.php
+++ prism.php
@@ -1,8 +1,17 @@
 <?php
 
+if(!isset($arg[0]))
+  $arg[0] = 'info';
+
 switch ($arg[0]){
-
-
+  case 'link':
+    echo 'https://github.com/TerrariaPrismTeam/Prism';
+    break;
+  case 'info':
+    echo 'Prism is modding API for Terraria 1.3 and above. Currently it's in development stage, check out progress here: https://github.com/TerrariaPrismTeam/Prism';
+    break;
+  default:
+    echo 'Prism is modding API for Terraria 1.3 and above. Currently it's in development stage, check out progress here: https://github.com/TerrariaPrismTeam/Prism';
 }
 
 ?>

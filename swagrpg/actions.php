4946d6c5156329c1a0da54598956418e0901e77f
MRokas <rokasmuningis@gmail.com>
2015-07-23 21:42:39 +0300

    Update actions.php

--- swagrpg/actions.php
+++ swagrpg/actions.php
@@ -1,7 +1,15 @@
 <?php
 
 function gainExp($p,$e){
-
+  $p['q']+=$e;
+  echo 'You have gained '.$e.' experience!';
+  if($p['q']>=$p['w']){
+    $p['q']-=$p['w'];
+    $p['l']++;
+    $p['w']*=1.1;
+    echo 'You have gained a level!';
+  }
+  die('');
 }
 
 ?>

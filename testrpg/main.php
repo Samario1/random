4162b76a861652c02a9989360341c7c90ca2f6ae
MRokas <rokasmuningis@gmail.com>
2015-07-23 15:16:22 +0300

    Create main.php

---
+++ testrpg/main.php
@@ -0,0 +1,17 @@
+<?php
+
+if(!isset($arg[0])){
+  if(isset($_STATE[$sender])){
+    die($sender.' : Level: 0');
+  }
+  die('Welcome to swagRPG, use join to start playing!');
+}
+switch ($arg[0]){
+  case "join":
+    $_STATE[$sender] = 'Joined';
+    break;
+  default:
+    die('');
+}
+
+?>

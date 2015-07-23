8b04f39aa6a1b6727d9e8899834d297e6bc8b562
MRokas <rokasmuningis@gmail.com>
2015-07-23 15:32:00 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -1,17 +1,31 @@
 <?php
 
 $_STATE = (array)$_STATE;
+$q = $arg[0];
+$w = $arg[1];
 
-if(!isset($arg[0])){
+if(!isset($q)){
   if(isset($_STATE[$sender])){
     die($sender.' : Level: 0');
   }
   die('Welcome to swagRPG, use join to start playing!');
 }
-switch ($arg[0]){
+switch ($q){
   case "join":
+    if(isset($_STATE[$sender])){
+      die('');
+    }
     $_STATE[$sender] = array();
     $_STATE[$sender]['status'] = 'Joined';
+    break;
+  case "pvp":
+    if(isset($w) && isset($_STATE[$w]) ){
+      die($sender.' has attacked '.$w;
+    }else if(!isset($w)){
+      die('You have to specify other player!');
+    }else if(!isset($_STATE[isset($w)])){
+      die('Player does not exist');
+    }
     break;
   default:
     die('');

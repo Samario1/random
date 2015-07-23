cd35e4e2b5901567b95ea7dc6d77e81b294115b8
MRokas <rokasmuningis@gmail.com>
2015-07-23 15:21:17 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -1,14 +1,15 @@
 <?php
 
 if(!isset($arg[0])){
-  if(isset($_STATE["{$sender}"])){
+  if(isset((array)$_STATE["{$sender}"])){
     die($sender.' : Level: 0');
   }
   die('Welcome to swagRPG, use join to start playing!');
 }
 switch ($arg[0]){
   case "join":
-    $_STATE[$sender] = 'Joined';
+    $_STATE[$sender] = array();
+    $_STATE[$sender]['status'] = 'Joined';
     break;
   default:
     die('');

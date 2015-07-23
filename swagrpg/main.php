e8d354c28b80063ecf93550dec71f5d1deab9eb0
MRokas <rokasmuningis@gmail.com>
2015-07-23 18:28:12 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -30,7 +30,7 @@ switch ($q){
     break;
   case "pve":
     die('Monster have yet to start spawning!');
-    case($w){
+    switch($w){
       case "forest":
         die('You are in the forest');
         break;

59cb4ebecb122000834d2d552ae89572d192d7f5
MRokas <rokasmuningis@gmail.com>
2015-07-23 13:55:56 +0300

    Create bosschallenge.php

---
+++ bosschallenge.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Author: https://github.com/MRokas
+ * Version: 0.1
+ * Description: smart ?bosschallenge for Shocky. 
+ *              No more copper shortsword against Moon Lord, on Last Prism against Eater of the Worlds.
+*/
+
+$BOSS_LIST = array(
+  "1" => array(
+    "King Slime",
+    "Eye of Cthulhu",
+    "Eater of the World",
+    "Brain of Cthulhu",
+    "Queen Bee"
+  ),
+  "2" => array(
+    "Skeletron",
+    "Wall of Flesh",
+  ),
+  "3" => array(
+    "The Destroyer",
+    "The Twins",
+    "Skeletron Prime"
+  ),
+  "4" => array(
+    "Plantera",
+    "Golem",
+  ),
+  "5" => array(
+    "Duke Fishron"
+  ),
+  "6" => array(
+    "Lunatic Cultist",
+    "Vortex Pillar",
+    "Nebula Pillar",
+    "Solar Pillar",
+    "Stardust Pillar",
+    "Moon Lord"
+  )
+);
+
+
+
+?>

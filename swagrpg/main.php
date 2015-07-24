85bedf448edddec8e2939ee84318eefe8f185b12
MRokas <rokasmuningis@gmail.com>
2015-07-24 16:18:43 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -1,11 +1,22 @@
 <?php
 
+function gainExp($p,$e){
+  echo "$sender slain blue slime in the forest! Exp gained: $e";
+  $p->q+=$e;
+  if($p->q >= $p->w){
+      $p->q -= $p->w;
+      $p->w*=1.1;
+      $p->l++;
+    }
+  return $p;
+}
+
 // User-data:
 // (j) => Joined
 // (l) => Level
 // (q) => Current Exp
 // (w) => Max Exp
-echo "TEST 0.0.08 <|> ";
+echo "TEST 0.0.10 <|> ";
 switch ($arg[0]){
   case "join":
     $_STATE->{$sender} = "{\"j\":1,\"l\":0,\"q\":0,\"w\":10}";
@@ -13,14 +24,8 @@ switch ($arg[0]){
   case "me":
     print_r($_STATE->{$sender});
     break;
-  case "q1":
-    $p = json_decode($_STATE->{$sender});
-    $p->q++;
-    if($p->q >= $p->w){
-      $p->q -= $p->w;
-      $p->w*=1.1;
-      $p->l++;
-    }
+  case "pve":
+    $p = gainExp(json_decode($_STATE->{$sender}),7);
     $_STATE->{$sender} = json_encode($p);
     break;
 }

0f84073ec98eabce8612af2a058aac94b43f32e8
MRokas <rokasmuningis@gmail.com>
2015-07-24 15:48:10 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -8,7 +8,7 @@
 
 switch ($arg[0]){
   case "join":
-    $_STATE->{$sender} = "[{"j":1,"l":0,"q":0,"w":10}]";
+    $_STATE->{$sender} = "[{\"j\":1,\"l\":0,\"q\":0,\"w\":10}]";
     break;
   case "me":
     print_r($_STATE->{$sender});

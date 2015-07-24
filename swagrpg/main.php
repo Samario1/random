f7612a8fde14947685bee247fa1dea05c3d2acbf
MRokas <rokasmuningis@gmail.com>
2015-07-24 16:23:04 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -8,6 +8,7 @@ function gainExp($p,$e){
       $p->q -= $p->w;
       $p->w*=1.1;
       $p->l++;
+      echo " | Congrulations, you've gained a level!";
     }
   return $p;
 }
@@ -17,9 +18,12 @@ function gainExp($p,$e){
 // (l) => Level
 // (q) => Current Exp
 // (w) => Max Exp
-echo "TEST 0.0.10 <|> ";
+echo "TEST 0.0.11 <|> ";
 switch ($arg[0]){
   case "join":
+    if(isset($_STATE->{$sender})){
+      die("You've already joined the game - use reset, and then join to start a fresh game!");
+    }
     $_STATE->{$sender} = "{\"j\":1,\"l\":0,\"q\":0,\"w\":10}";
     break;
   case "me":

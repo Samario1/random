e614e5adee893efc0b320560ad3d6384137f7486
MRokas <rokasmuningis@gmail.com>
2015-07-24 15:52:39 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -5,13 +5,13 @@
 // (l) => Level
 // (q) => Current Exp
 // (w) => Max Exp
-echo "TEST 0.0.01 <|> ";
+echo "TEST 0.0.02 <|> ";
 switch ($arg[0]){
   case "join":
     $_STATE->{$sender} = "[{\"j\":1,\"l\":0,\"q\":0,\"w\":10}]";
     break;
   case "me":
-    print_r(json_decode($_STATE->{$sender}));
+    print_r(json_decode($_STATE->{$sender},true));
     break;
 }
 ?>

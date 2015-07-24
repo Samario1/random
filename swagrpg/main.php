bc045cf5c71cbac32e14d5dc623e278f93ef773f
MRokas <rokasmuningis@gmail.com>
2015-07-24 15:55:14 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -13,5 +13,12 @@ switch ($arg[0]){
   case "me":
     print_r(json_decode($_STATE->{$sender},true));
     break;
+  case "q1":
+    $_STATE->{$sender}->q++;
+    if($_STATE->{$sender}->q >= $_STATE->{$sender}->w){
+      $_STATE->{$sender}->q -= $_STATE->{$sender}->w;
+      $_STATE->{$sender}->w*=1.1;
+    }
+    break;
 }
 ?>

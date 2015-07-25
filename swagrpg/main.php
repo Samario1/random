a5f6291faa81cc3aa026dfd1d60ee9c89e697f32
Samario1 <peter.tarn2001@gmail.com>
2015-07-25 20:59:07 +0100

    pls

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -33,7 +33,10 @@ $MOBS = array(
   10 => array("name" => "Zombie Eskimo"),
   11 => array("name" => "Demon Eye"),
   12 => array("name" => "Hellbat"),
-  13 => array("name" => "Granite Elemental")
+  13 => array("name" => "Granite Elemental"),
+  14 => array("name" => "Hornet"),
+  15 => array("name" => "Man Eater"),
+  16 => array("name" => "Spiked Jungle Slime")
 );
 
 $BIOMES = array(
@@ -79,6 +82,16 @@ $BIOMES = array(
     "mobs" => array(
       "day"   => array(4,12,13),
       "night" =>  array(4,12,13)
+    ),
+      "jungle" => array(
+    "lvl" => 15,
+    "exp" => 16,
+    "gold_min" => 115,
+    "gold_max" => 310,
+    "rest" => 105,
+    "mobs" => array(
+      "day"   => array(14,15,16),
+      "night" =>  array(14,15)
     )
   )
   );

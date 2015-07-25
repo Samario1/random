aa6369a7af8b40cda819a3861a17ee265327984a
MRokas <rokasmuningis@gmail.com>
2015-07-25 18:01:37 +0300

    Update main.php

--- swagrpg/main.php
+++ swagrpg/main.php
@@ -1,6 +1,6 @@
 <?php
 
-$MOBS = array(
+$GLOBALS["MOBS"] = array(
   0 =>  array("name" => "Blue Slime"),
   1 =>  array("name" => "Green Slime"),
   2 =>  array("name" => "Ice Slime"),
@@ -17,7 +17,7 @@ $MOBS = array(
   13 => array("name" => "Granite Elemental")
 );
 
-$BIOMES = array(
+$GLOBALS["BIOMES"] = array(
   "forest" => array(
     "lvl" => 0,
     "exp" => 5,
@@ -117,7 +117,7 @@ function game_time(){
  */
 function gainExp($p,$m,$b,$e,$n){
   global $sender;
-  global $MOBS;
+  //global $MOBS;
   $p->q+=round($e*($n?2:1));
   $enm = $MOBS[$b['mobs'][$n?"night":"day"][array_rand($b['mobs'][$n?"night":"day"])]];
   $enm["name"] = (in_array($enm["name"][0],array('A','E','I','O','U'))?"an ":"a ").$enm["name"];
@@ -142,7 +142,7 @@ function gainExp($p,$m,$b,$e,$n){
  function action_pve($p,$b){
   global $sender;
   global $_STATE;
-  global $BIOMES;
+  //global $BIOMES;
   $time = game_time();
   if( floatval(str_replace(":",".",$time["F"])) > 19.3 or floatval(str_replace(":",".",$time["F"])) < 4.3 ){
     $is_night = true;
